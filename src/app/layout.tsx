caa4ed5a1e41089a625cb533428d6d4ed71a542c
maceoCK <maceo@vt.edu>
2024-08-22 16:36:07 -0400

    posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,33 +8,38 @@ import { ourFileRouter } from "./api/uploadthing/core";
 import { extractRouterConfig } from "uploadthing/server";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { Toaster } from "sonner";
+import { CSPostHogProvider } from "./_analytics/provider";
 
 export const metadata: Metadata = {
-  title: "T3 Image Gallery",
-  description: "Image Gallery made with t3",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+    title: "T3 Image Gallery",
+    description: "Image Gallery made with t3",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
-  children,
-  modal
-}: Readonly<{ children: React.ReactNode, modal: React.ReactNode }>) {
-  return (
-    <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable}`}>
-        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
-        <body className="flex min-h-screen flex-col gap-y-4 dark">
-          <div className="h-screen grid grid-rows-[auto_1fr]">
-            <TopNav />
-            <main className="overflow-y-scroll">
-              {children}
-            </main>
-          </div>
-          {modal}
-          <div id="modal-root" />
-          <Toaster position="bottom-right" theme="dark" />
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+    children,
+    modal,
+}: Readonly<{ children: React.ReactNode; modal: React.ReactNode }>) {
+    return (
+        <ClerkProvider>
+            <CSPostHogProvider>
+                <html lang="en" className={`${GeistSans.variable}`}>
+                    <NextSSRPlugin
+                        routerConfig={extractRouterConfig(ourFileRouter)}
+                    />
+                    <body className="flex min-h-screen flex-col gap-y-4 dark">
+                        <div className="h-screen grid grid-rows-[auto_1fr]">
+                            <TopNav />
+                            <main className="overflow-y-scroll">
+                                {children}
+                            </main>
+                        </div>
+                        {modal}
+                        <div id="modal-root" />
+                        <Toaster position="bottom-right" theme="dark" />
+                    </body>
+                </html>
+            </CSPostHogProvider>
+        </ClerkProvider>
+    );
 }
