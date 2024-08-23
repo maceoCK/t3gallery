199fdde4efa7c1524ec1b6bd291fdde4a27d7a90
maceoCK <maceo@vt.edu>
2024-08-23 19:37:27 -0400

    limit who can upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,37 +1,43 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
 import { images } from "~/server/db/schema";
 import { ratelimit } from "~/server/ratelimit";
- 
+
 const f = createUploadthing();
- 
- 
+
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 12 } })
-    .middleware(async ({ req }) => {
-      const user = auth();
- 
-      if (!user.userId) throw new UploadThingError("Unauthorized");
+    imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 12 } })
+        .middleware(async ({ req }) => {
+            const user = auth();
 
-      const { success } = await ratelimit.limit(user.userId);
-      if (!success) throw new UploadThingError("Rate limit exceeded");
- 
-      return { userId: user.userId };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      console.log("Upload complete for userId:", metadata.userId);
- 
-      await db.insert(images).values({
-        name: file.name,
-        url: file.url,
-        userId: metadata.userId ?? '',
-      });
+            if (!user.userId) throw new UploadThingError("Unauthorized");
+            const fullUserData = await clerkClient().users.getUser(user.userId);
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
-    }),
+            if (!fullUserData) throw new UploadThingError("User not found");
+            if (fullUserData.privateMetadata?.["can-upload"] !== true)
+                throw new UploadThingError(
+                    "User does not have permission to upload",
+                );
+
+            const { success } = await ratelimit.limit(user.userId);
+            if (!success) throw new UploadThingError("Rate limit exceeded");
+
+            return { userId: user.userId };
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            console.log("Upload complete for userId:", metadata.userId);
+
+            await db.insert(images).values({
+                name: file.name,
+                url: file.url,
+                userId: metadata.userId ?? "",
+            });
+
+            // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+            return { uploadedBy: metadata.userId };
+        }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
